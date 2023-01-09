3c2e11cbb041861f622254512a0a2014b65edbfd
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-09 18:09:51 -0400

    fix linter errors

--- book.js
+++ book.js
@@ -1,51 +1,51 @@
 const form = document.querySelector('.form');
 const { title, author } = form.elements;
 
-let data = localStorage.getItem('library');
+const data = localStorage.getItem('library');
 let library = JSON.parse(data);
 const loadBooks = () => {
-	const bookWraper = document.querySelector('.bookWraper');
-	let bookElement = '';
-	library.forEach((book) => {
-		bookElement += `
+  const bookWraper = document.querySelector('.bookWraper');
+  let bookElement = '';
+  library.forEach((book) => {
+    bookElement += `
                 <div class="book">
                 <div id="book-title">${book.bookTitle}</div>
                 <div id="book-author">${book.bookAuthor}</div>
                 <button id="remove">Remove</button>
                 </div>
                 <hr> `;
-	});
-	bookWraper.innerHTML = bookElement;
+  });
+  bookWraper.innerHTML = bookElement;
 };
 
 loadBooks();
 
 const remove = (index) => {
-	library = library.filter((el, i) => i !== index);
-	const libraryStringify = JSON.stringify(library);
-	localStorage.setItem('library', libraryStringify);
-	location.reload();
+  library = library.filter((el, i) => i !== index);
+  const libraryStringify = JSON.stringify(library);
+  localStorage.setItem('library', libraryStringify);
+  window.location.reload();
 };
 
 const addEventOnButton = () => {
-	const listButtonRemove = document.querySelectorAll('#remove');
-	listButtonRemove.forEach((button, index) => {
-		button.addEventListener('click', () => remove(index));
-	});
+  const listButtonRemove = document.querySelectorAll('#remove');
+  listButtonRemove.forEach((button, index) => {
+    button.addEventListener('click', () => remove(index));
+  });
 };
 
 addEventOnButton();
 
 const addBook = (e) => {
-	e.preventDefault();
-	const bookTitle = title.value;
-	const bookAuthor = author.value;
-	const book = { bookTitle, bookAuthor };
-	library.push(book);
-	const libraryStringify = JSON.stringify(library);
-	localStorage.setItem('library', libraryStringify);
-	location.reload();
-	form.reset();
+  e.preventDefault();
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  const book = { bookTitle, bookAuthor };
+  library.push(book);
+  const libraryStringify = JSON.stringify(library);
+  localStorage.setItem('library', libraryStringify);
+  window.location.reload();
+  form.reset();
 };
 
 form.addEventListener('submit', addBook);
