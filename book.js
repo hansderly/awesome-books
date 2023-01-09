67d8118de9f1675e13374a4adb561cc3927de26b
unknown <Lucas@DESKTOP-2628.2628>
2023-01-09 14:30:30 -0500

    add display on the page function

--- book.js
+++ book.js
@@ -8,7 +8,30 @@ const addBook = (e) => {
   const bookAuthor = author.value;
   const book = { bookTitle, bookAuthor };
   library.push(book);
+
+  let newBook = document.createElement('div');
+    string = `<div id="book-title">${title.value}</div>
+    <div id="book-author">${author.value}</div>
+    <button id="remove">Remove</button>
+    <hr></hr>`;
+    newBook.innerHTML = string;
+    document.querySelector('.bookWraper').appendChild(newBook);
+
 };
+
+const loadBooks = (e) => {
+
+    
+    const container =  document.querySelector('.bookWraper');
+    string = `
+    <div class="book"> 
+    <div id="book-title">${title.value}</div>
+    <div id="book-author">${author.value}</div>
+    <button id="remove">Remove</button>
+    <hr>
+    </div>`;
+    container.innerHTML = string;
+}
 
 const buttonRemove = document.querySelector('#remove');
 
