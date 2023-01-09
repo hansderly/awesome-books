d81b23dfafc678a5faf83e4b4e4182dad2516b4c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-09 13:49:19 -0400

    add add functionality

--- book.js
+++ book.js
@@ -1,2 +1,13 @@
-const book = [];
+const form = document.querySelector('.form');
+const { title, author } = form.elements;
+const library = [];
 
+const addBook = (e) => {
+  e.preventDefault();
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  const book = { bookTitle, bookAuthor };
+  library.push(book);
+};
+
+form.addEventListener('submit', addBook);
