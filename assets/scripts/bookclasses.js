f96885aa68a45bbf4c3cd873ca8c1d7a7bfc526a
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-10 16:43:49 -0400

    add add and remove funciton to the library class and also getter and setter

--- assets/scripts/bookclasses.js
+++ assets/scripts/bookclasses.js
@@ -1,11 +1,29 @@
-class Book{
-    constructor(author,title){
-        this.author = author;
-        this.title = title;
-    }
+/* eslint-disable max-classes-per-file */
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-class library{
-    
-}
+class Library {
+  constructor() {
+    this.books = [];
+  }
 
+  add(book) {
+    this.books.push(book);
+  }
+
+  remove(id) {
+    this.books = this.books.filter((el, i) => i !== id);
+  }
+
+  getList() {
+    return this.books;
+  }
+
+  setList(listBook) {
+    this.books = listBook;
+  }
+}
