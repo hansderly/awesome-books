536842c02bc60ddf203a08f6320800621a661c24
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-10 20:43:37 -0400

    add new logic for removing book

--- assets/scripts/bookclasses.js
+++ assets/scripts/bookclasses.js
@@ -19,65 +19,54 @@ class Library {
     this.books = this.books.filter((el, i) => i !== id);
   }
 
-  getList() {
-    return this.books;
-  }
-
   setList(listBook) {
     this.books = listBook;
   }
 }
 
-
-
-const form = document.querySelector('.form');
-
-const { title, author } = form.elements;
-
-const data = localStorage.getItem('library');
-
 const library = new Library();
 
-library.setList(JSON.parse(data));
+const form = document.querySelector('.form');
+const { title, author } = form.elements;
+
+const data = JSON.parse(localStorage.getItem('library'));
+if (data !== null) library.setList(data.books);
 
 const saveTolocalStorage = () => {
-    const libraryStringify = JSON.stringify(library);
-    localStorage.setItem('library', libraryStringify);
-  };
+  const libraryStringify = JSON.stringify(library);
+  localStorage.setItem('library', libraryStringify);
+};
 
-  const loadBooks = () => {
-    const bookWraper = document.querySelector('.bookWraper');
-    let bookElement = '';
-    const newlist = library.getList();
-    newlist.forEach((book, index) => {
-      bookElement += `
-                  <div class="book">
-                  <div id="book-title">${book.bookTitle} by ${book.bookAuthor}</div>
-                  <button id="remove" onclick="remove(${index})" >Remove</button>
-                  </div>
-                  <hr> `;
-    });
-    bookWraper.innerHTML = bookElement.length === 0 ? '<p> No book </p> <hr><br>' : bookElement;
-  };
+const loadBooks = () => {
+  const bookWraper = document.querySelector('.bookWraper');
+  let bookElement = '';
+  library.books.forEach((book, index) => {
+    bookElement += `
+                <div class="book">
+                <div id="book-title">${book.title} by ${book.author}</div>
+                <button id="remove" onclick="remove(${index})" >Remove</button>
+                </div>
+                <hr> `;
+  });
+  bookWraper.innerHTML = bookElement.length === 0 ? '<p> No book </p> <hr><br>' : bookElement;
+};
+loadBooks();
+
+const remove = (index) => {
+  library.remove(index);
+  saveTolocalStorage();
   loadBooks();
+};
+remove();
 
-  const remove = (index) => {
-    library.remove(index);
-    loadBooks();
-  };
-  remove();
-
-  const addBook = (e) => {
-    e.preventDefault();
-    const bookTitle = title.value;
-    const bookAuthor = author.value;
-
-    const book = new Book(bookTitle, bookAuthor);
-
-    library.add(book);
-    saveTolocalStorage();
-    loadBooks();
-    form.reset();
-  };
-  form.addEventListener('submit', addBook);
-  
+const addBook = (e) => {
+  e.preventDefault();
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  const book = new Book(bookTitle, bookAuthor);
+  library.add(book);
+  saveTolocalStorage();
+  loadBooks();
+  form.reset();
+};
+form.addEventListener('submit', addBook);
